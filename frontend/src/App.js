58240c73564b8f9af3a222e0764a766bfcc3610d
Devan Eisnor <W0196059@nscc.ca>
2025-03-19 13:46:27 -0300

    Border updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,11 @@ function App() {
   return (
     <div className="bg-light min-vh-100">
       {/* Header */}
-      <nav className="navbar navbar-dark bg-primary">
+      <nav className="navbar navbar-dark bg-primary py-4">
         <div className="container">
-          <span className="navbar-brand mb-0 h1">Faculty Time Report System</span>
+          <span className="navbar-brand mb-0 fw-bold fs-1">
+            NSCC Faculty Time Report System
+          </span>
         </div>
       </nav>
 
