e05d4934e4a8caab36e409662b71701e69963e10
Devan Eisnor <W0196059@nscc.ca>
2025-03-19 13:41:28 -0300

    installed bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,18 @@ import AdminDashboard from "./AdminDashboard";
 
 function App() {
   return (
-    <div>
-      <AdminDashboard />
+    <div className="bg-light min-vh-100">
+      {/* Header */}
+      <nav className="navbar navbar-dark bg-primary">
+        <div className="container">
+          <span className="navbar-brand mb-0 h1">Faculty Time Report System</span>
+        </div>
+      </nav>
+
+      {/* Main Content */}
+      <div className="container mt-4">
+        <AdminDashboard />
+      </div>
     </div>
   );
 }
